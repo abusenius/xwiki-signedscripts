0d997c1e5f18ed167211c17cdf16ecd7bc789791
Alex Busenius <alex.busenius@googlemail.com>
2010-08-20 18:22:39 +0200

    DEBUG Trying to control PR rights with SignedMacroState

--- xwiki-rendering-macro-signed/src/main/java/org/xwiki/rendering/internal/macro/signed/SignedMacro.java
+++ xwiki-rendering-macro-signed/src/main/java/org/xwiki/rendering/internal/macro/signed/SignedMacro.java
@@ -26,6 +26,7 @@ import java.util.List;
 import org.xwiki.component.annotation.Component;
 import org.xwiki.component.annotation.Requirement;
 import org.xwiki.component.manager.ComponentManager;
+import org.xwiki.signed.SignedMacroState;
 import org.xwiki.signedscripts.ScriptSigner;
 import org.xwiki.signedscripts.SignedScript;
 import org.xwiki.rendering.block.Block;
@@ -38,18 +39,37 @@ import org.xwiki.rendering.transformation.MacroTransformationContext;
 /**
  * Macro containing a signed script. Verifies the content on view, does not allow further execution if the verification
  * fails.
+ * <p>
+ * Note that there are 2 instances of this implementation, one for macro role and one for state role. Both instances
+ * share the same thread local storage, resulting in consistent state value. The storage located in the macro
+ * implementation allows to expose only the {@link #isSigned()} method and leave {@link #setSigned(boolean)} private,
+ * which guarantees that the state can only be changed from this class or using java.unsafe (that needs PR).</p>
  * 
  * @version $Id$
  * @since 2.5
  */
 @Component("signed")
-public class SignedMacro extends AbstractMacro<Object>
+public class SignedMacro extends AbstractMacro<Object> implements SignedMacroState
 {
     /** The description of the macro. */
     private static final String DESCRIPTION = "Executes a signed script with high privileges.";
 
     /** The description of the macro content. */
     private static final String CONTENT_DESCRIPTION = "the signature and a script macro";
+
+    /** Shared execution state of the signed macro. */
+    private static ThreadLocal<Boolean> inSignedState = new ThreadLocal<Boolean>() {
+        /**
+         * {@inheritDoc}
+         * 
+         * @see java.lang.ThreadLocal#initialValue()
+         */
+        @Override
+        protected Boolean initialValue()
+        {
+            return Boolean.FALSE;
+        }
+    };
 
     /** Used to get the parser for the syntax of the current document. */
     @Requirement
@@ -111,5 +131,27 @@ public class SignedMacro extends AbstractMacro<Object>
         } catch (Exception exception) {
             throw new MacroExecutionException("Evaluation of a signed script failed. ", exception);
         }
+    }
+
+    /**
+     * {@inheritDoc}
+     * 
+     * @see org.xwiki.signed.SignedMacroState#isSigned()
+     */
+    public boolean isSigned()
+    {
+        return inSignedState.get().booleanValue();
+    }
+
+    /**
+     * Change the signed state of the context.
+     * FIXME test for race conditions!
+     * 
+     * @param state new state
+     */
+    public synchronized void setSigned(boolean state)
+    {
+        System .out.println("SET SIGNED:  " + state);
+        inSignedState.set(Boolean.valueOf(state));
     }
 }
---
+++ xwiki-rendering-macro-signed/src/main/java/org/xwiki/signed/SignedMacroState.java
@@ -0,0 +1,48 @@
+/*
+ * See the NOTICE file distributed with this work for additional
+ * information regarding copyright ownership.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+package org.xwiki.signed;
+
+import org.xwiki.component.annotation.ComponentRole;
+
+
+/**
+ * Component used to check the execution state of signed macros.
+ * 
+ * @version $Id$
+ * @since 2.5
+ */
+@ComponentRole
+public interface SignedMacroState
+{
+    /**
+     * Check if the code is executing in a signed context.
+     * 
+     * @return true if currently executed code was called from inside of a valid signed macro, false otherwise
+     */
+    boolean isSigned();
+
+    /**
+     * Change the signed state of the context.
+     * 
+     * @param state new state
+     */
+    void setSigned(boolean state);
+}
+
